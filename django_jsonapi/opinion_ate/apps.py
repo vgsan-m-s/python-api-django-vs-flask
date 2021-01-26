da6731fef31f2784c08ba2638298b36387146fad
Vageesan Magenthiran <Vageesan.Magenthiran@acronis.com>
2021-01-26 15:03:27 +0800

    VGSAN :: Initial commit.

---
+++ django_jsonapi/opinion_ate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OpinionAteConfig(AppConfig):
+    name = 'opinion_ate'
---
+++ django_jsonapi/opinion_ate/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from opinion_ate.models import Restaurant, Dish
+from opinion_ate.serializers import RestaurantSerializer, DishSerializer
+from rest_framework import viewsets
+
+class RestaurantViewSet(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+class DishViewSet(viewsets.ModelViewSet):
+    queryset = Dish.objects.all()
+    serializer_class = DishSerializer
